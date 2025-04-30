ad08c8b3517cbd187d1367edc2f8d9f52a74be81
hamed <52428565+hamednourzaei@users.noreply.github.com>
2025-04-30 13:16:24 +0330

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import QueryProvider from "@/components/Reactquery/QueryProvider";
 import "./globals.css"
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fa">
